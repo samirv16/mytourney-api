02247c25984923957869ff4b7a0e63bcb07dfdf1
samirv16 <samir.vallecillo@outlook.com>
2020-01-23 11:44:19 -0600

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Team.create([
+    { name: 'Barca'},
+    { name: 'NAFC'},
+    { name: 'Liverpool'},
+    { name: 'Madrid'},
+    { name: 'PSG'},
+    { name: 'Juve'},
+    { name: 'Milan'},
+    { name: 'Chelsea'},
+])
